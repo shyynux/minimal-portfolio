7ce00be14b0b107fdf0b74f4444877a75f7fb8d3
shyynux <128243259+shyynux@users.noreply.github.com>
2025-05-21 00:08:08 +0530

    Update about.tsx

--- bio/src/pages/about.tsx
+++ bio/src/pages/about.tsx
@@ -2,30 +2,22 @@ import Footer from "../assets/components/footer";
 import Header from "../assets/components/header";
 
 const About = () => {
-    return (
-        <>
-        <div className="">
-        <Header />
-        <div className='
-    font-mono text-zinc-500
-    flex flex-col items-center justify-center h-screen
-    '>
-        <div className="w-1/2">
-        <p className='justify-content'>
-            <p>
-            I like building things on the internet.
-            </p>
-      dm or email me at
-      (shyynux@gmail.com).
+  return (
+    <>
+      <Header />
+      <div className="font-mono text-zinc-500 flex flex-col items-center justify-center min-h-screen">
+        <div className="w-1/2 text-center">
+          <p>I like building things on the internet.</p>
+          <p>
+            dm or email me at <a href="mailto:shyynux@gmail.com">shyynux@gmail.com</a>.
+          </p>
         </div>
         <div className="pt-8">
-        <Footer />
+          <Footer />
         </div>
-        </div>
-        </div>
-        </>
-    )
+      </div>
+    </>
+  );
 };
-
 
 export default About;
