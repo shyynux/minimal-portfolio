073ff806f3791a521e8fc66d4decfbb2a0103bba
shyynux <128243259+shyynux@users.noreply.github.com>
2025-05-20 23:54:37 +0530

    Update about.tsx

--- bio/src/pages/about.tsx
+++ bio/src/pages/about.tsx
@@ -13,29 +13,10 @@ const About = () => {
         <div className="w-1/2">
         <p className='justify-content'>
             <p>
-            I have around 2 years of professional work experience,
-            apart from that I have done internships and have been 
-            coding on and off since years as i have a passion for 
-            building stuff. I was also good academically (if that matters) 
-            in school and college. 
+            I like building things on the internet.
             </p>
-      <br />
-      Passionate about web development,
-      I get obessessed with things and then drown
-           myself in it, currently it is building more and
-           more. <br />
-       I also enjoy filming videos and reading books, 
-      <br />
-       especially those with unique perspectives.
-       <br />
-      </p>
-      <br />
-      I usually work with
-      react, typescript, javascript, NodeJS and TailwindCSS.
-      <br />
-      <br />
-      I am open for freelance projects, dm or email me
-      (shyynux@gmail.com) with your work.
+      dm or email me at
+      (shyynux@gmail.com).
         </div>
         <div className="pt-8">
         <Footer />
